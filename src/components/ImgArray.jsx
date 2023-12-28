7974faea2bcb96eeb6f43486834fb778b6b65229
Jose Manuel Vera Rodriguez <127628495+jomanuelvr@users.noreply.github.com>
2023-12-28 04:56:19 -0300

    Update ImgArray.jsx

--- src/components/ImgArray.jsx
+++ src/components/ImgArray.jsx
@@ -3,7 +3,7 @@ const ImageData = () => {
       {
         characterName: 'Dr. Frink',
         imageUrl: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/86cf185c-ad61-4faa-9c63-de77f6c62a3c/dazued3-0d2ebd11-1a0b-40aa-b983-118388120c85.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwic3ViIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsImF1ZCI6WyJ1cm46c2VydmljZTpmaWxlLmRvd25sb2FkIl0sIm9iaiI6W1t7InBhdGgiOiIvZi84NmNmMTg1Yy1hZDYxLTRmYWEtOWM2My1kZTc3ZjZjNjJhM2MvZGF6dWVkMy0wZDJlYmQxMS0xYTBiLTQwYWEtYjk4My0xMTgzODgxMjBjODUucG5nIn1dXX0._Dd1n9JLBo-84LqXLRHNDcGDIEbzaVGDa7ULaPL5Y1Y',
-        phrase: 'Como pueden ver la contracción y expansión de las ondas longitudinales, causan la oscilación errática de las partículas cercanas... Sí, ¿Tino? ... No, no lo dsfrutarían al nivel que yo lo disfruto... jajaja ¡Miren esos colores!'
+        phrase: 'Como pueden ver la contracción y expansión de las ondas longitudinales, causan la oscilación errática de las partículas cercanas... Sí, ¿Tino? ... No, no lo disfrutarían al nivel que yo lo disfruto... jajaja ¡Miren esos colores!'
       },
       {
         characterName: 'Homero Simpson',
@@ -13,7 +13,7 @@ const ImageData = () => {
       {
         characterName: 'Maggie Simpson',
         imageUrl: 'https://3.bp.blogspot.com/-Bpo8wg3XPtA/V7pK777xo1I/AAAAAAABBT8/HcHPB-qYLQMb5ULiws280bwbDBVXzjpMQCPcB/s1600/maggie.png',
-        phrase: 'El universo es muy confuso realmente'
+        phrase: 'El universo es muy confuso realmente.'
       },
       {
         characterName: 'Marge Simpson',
@@ -23,12 +23,12 @@ const ImageData = () => {
       {
         characterName: 'Otto',
         imageUrl: 'https://2.bp.blogspot.com/-020IlzmwCFs/V7pK7zhkG5I/AAAAAAABBT8/Z0zIiFzVdo4v5wToz6OZVXqPSDM4dSWgACPcB/s1600/otto.png',
-        phrase: 'Eso no es una canción. Las canciones hablan de pactos con el diablo, tierras lejanas y el humo en relación con el agua'
+        phrase: 'Eso no es una canción. Las canciones hablan de pactos con el diablo, tierras lejanas y el humo en relación con el agua.'
       },
       {
         characterName: 'Lisa Simpson',
         imageUrl: 'https://2.bp.blogspot.com/-B5MH6BOtgIU/V7pK77ullFI/AAAAAAABBT8/KLJUQ5bIRakKDUL8IB2yPz8MfclaWFgRgCPcB/s1600/lisa2.png',
-        phrase: 'Ya no tiene que comer carne, hice gazpacho para todos!!!'
+        phrase: 'Ya no tienen que comer carne, hice gazpacho para todos!!!'
       }
       ,
       {
