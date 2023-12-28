846c6da935950da38747a976ad154ac2005a588f
José Vera <profe.jomanuelvr@gmail.com>
2023-12-28 12:25:13 -0300

    se resolvio el problema con quitar el div del boton

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -5,19 +5,29 @@ import { Button } from "react-bootstrap";
 
 const CharacterCard = ({ randomData, onCardSelect, handleButtonClick }) => {
   return (
-    <div className="container">
-      <h2 className="text-center simpsonfont">{randomData?.characterName}</h2>
-      <hr />
-      <img className="imgCharacters" src={randomData?.imageUrl} alt="Imagen del personaje" />
-      <div className="">
-        <p className="txFrases">{randomData?.phrase}</p>
-      </div>     
-    </div>
-     <div className="text-center ">
-        <Button variant="warning" className="btnAjust" onClick={handleButtonClick}>
+    <>
+      <div className="container">
+        <h2 className="text-center simpsonfont">{randomData?.characterName}</h2>
+        <hr />
+        <img
+          className="imgCharacters"
+          src={randomData?.imageUrl}
+          alt="Imagen del personaje"
+        />
+        <div className="">
+          <p className="txFrases">{randomData?.phrase}</p>
+        </div>
+      </div>{" "}
+      <div className="text-center ">
+        <Button
+          variant="warning"
+          className="btnAjust"
+          onClick={handleButtonClick}
+        >
           Obtener frase
         </Button>
       </div>
+    </>
   );
 };
 
